513ce6e4d7be9d77274ef927bf2af3ea43f624a7
naterocks49 <108183408+naterocks49@users.noreply.github.com>
2023-05-04 21:26:32 -0400

    Updates

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
+import 'bulma/css/bulma.css'
 import React from 'react';
 import { createRoot } from'react-dom/client';
 import App from './App';
+import { store } from "./store";
+import { Provider } from "react-redux";
+import "./styles.css"
 
 const el = document.getElementById('root');
 const root = createRoot(el);
 
-root.render(<App />);
+root.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+    );
